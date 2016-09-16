b09ddf96ec65d229ab74b99621533526973ab421
arctg70 <simon.zhou@gmail.com>
2016-09-16 22:31:59 +0800

    傅立叶变换 频域分析 2D傅里叶变换 图像处理

---
+++ fft.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+#coding=utf-8
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+from scipy import fftpack
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
+import numpy as np
+
+img=np.array(Image.open('hole.png').convert('L'))
+
+
+print img.shape
+print img.shape[0]
+sig_fft=fftpack.fft2(img)
+fshift=np.fft.fftshift(sig_fft)
+#  fshift=sig_fft
+#  noisex=200
+#  noisey=290
+#  fshift[:noisex,:]=0
+#  fshift[:,:noisey]=0
+#  fshift[-1*noisex:,:]=0
+#  fshift[:,-1*noisey:]=0
+fshift2=np.fft.fftshift(fshift)
+imgrr=np.abs(fftpack.ifft2(fshift2))
+
+
+#  print fshift.shape
+fft2_orgimg=np.abs(fshift)
+fft2_img=np.log(np.abs(fshift))
+fig = plt.figure()
+#  ax = fig.gca(projection='3d')
+#  X = np.arange(0, 470,1)
+#  Y = np.arange(0,630, 1)
+#  X, Y = np.meshgrid(X, Y)
+#  Z = fft2_img[X,Y]
+#  surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
+                                 #  linewidth=0, antialiased=False)
+#
+#  plt.show()
+plt.imshow(fft2_img,'gray')
+
+#  print fft2_orgimg
+#
+#  plt.imshow(imgrr,'gray')
+plt.colorbar()
+
+
+#  plt.imshow(img,'gray')
+#  plt.axis('off')
+
+plt.show()
+
+#  plt.gca().axis('off')
+
--- img.py
+++ img.py
@@ -3,19 +3,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-img=np.array(Image.open('mandelbrot.jpg'))
+img=np.array(Image.open('img2.jpg').convert('L'))
 #img_file=cbook.get_sample_data('~/git/testproject/lip.jpg')
 #image= plt.imread(img_file)
 print img.shape
-img[:,:,0]=0     #red
-img[:,:,1]=0     #green
+#img[:,:,0]=0     #red
+#img[:,:,1]=0     #green
 #  img[:,:,2]=0     #blue
 #  img[:,:,0]=(img[:,:,0]+img[:,:,1]+img[:,:,2])/3
 #  img[:,:,1]=img[:,:,0]
 #  img[:,:,2]=img[:,:,0]
 #plt.figure('lip')
 #  print imgclip.shape
+img[:,:]=255
+img[0:300,:]=0
+img[-300:,:]=0
+img[:,0:300]=0
+img[:,-300:]=0
 plt.imshow(img)
+plt.imsave('hole.png',img  )
 plt.axis('off')
 
 #  pil_im=Image.fromarray(imgclip)
